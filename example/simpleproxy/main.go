c4e0ca017c8b389409d5d2e4c8df20ddcd3d2136
Zichao Li <zichao@haxii.com>
2017-11-18 12:28:00 +0800

    code refactoring on handler

--- example/simpleproxy/main.go
+++ example/simpleproxy/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"net"
+	"os"
+	"sync"
 
+	"github.com/haxii/fastproxy/header"
 	"github.com/haxii/fastproxy/proxy"
 )
 
@@ -11,5 +16,60 @@ func main() {
 	if err != nil {
 		return
 	}
-	proxy.NewSimpleProxy().Serve(ln)
+	proxy := proxy.Proxy{}
+	proxy.Serve(ln)
+}
+
+//SimpleSnifferPool implements the SnifferPool based on simpleSniffer & sync.Pool
+type SimpleSnifferPool struct {
+	pool sync.Pool
+}
+
+//Get get a simple sniffer from pool
+func (p *SimpleSnifferPool) Get(addr net.Addr) proxy.Sniffer {
+	v := p.pool.Get()
+	if v == nil {
+		sniffer := &simpleSniffer{clientAddr: addr.String()}
+		return sniffer
+	}
+	sniffer := v.(*simpleSniffer)
+	sniffer.clientAddr = ""
+	return sniffer
+}
+
+//Put puts a simple sniffer back to pool
+func (p *SimpleSnifferPool) Put(s proxy.Sniffer) {
+	p.pool.Put(s)
+}
+
+type simpleSniffer struct {
+	clientAddr string
+}
+
+func (s *simpleSniffer) GetRequestWriter(uri []byte, header header.Header) io.Writer {
+	fmt.Printf(`
+************************
+addr:%s
+************************
+request uri:%s
+************************
+content length:%d
+************************
+`,
+		s.clientAddr, uri, header.ContentLength())
+	return os.Stdout
+}
+
+func (s *simpleSniffer) GetResponseWriter(statusCode int, header header.Header) io.Writer {
+	fmt.Printf(`
+************************
+addr:%s
+************************
+response status code:%d
+************************
+content length:%d
+************************
+`,
+		s.clientAddr, statusCode, header.ContentLength())
+	return os.Stdout
 }
