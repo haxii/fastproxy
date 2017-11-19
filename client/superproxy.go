4997e66fa58f019f779b67edf345f56d392da718
Zichao Li <zichao@haxii.com>
2017-11-19 23:15:10 +0800

    added superproxy response read

--- client/superproxy.go
+++ client/superproxy.go
@@ -1,10 +1,12 @@
 package client
 
 import (
+	"bytes"
 	"crypto/tls"
 	"encoding/base64"
 	"errors"
 	"fmt"
+	"io"
 	"net"
 
 	"github.com/haxii/fastproxy/bufiopool"
@@ -72,7 +74,7 @@ func (p *SuperProxy) DialFunc(pool *bufiopool.Pool) transport.DialFunc {
 			c.Close()
 			return nil, err
 		}
-		if err = p.readProxyReq(c, pool); err != nil {
+		if err = p.readProxyResp(c, pool); err != nil {
 			c.Close()
 			return nil, err
 		}
@@ -85,9 +87,7 @@ var (
 	superProxyReqProtocol   = []byte("HTTP/1.1")
 	superProxyReqHostHeader = []byte("Host:")
 	superProxyReqSP         = []byte(" ")
-	lensuperProxyReqSP      = 1
 	superProxyReqCRLF       = []byte("\r\n")
-	lensuperProxyReqCRLF    = 2
 )
 
 //writeProxyReq write proxy `CONNECT` header to proxy connection,
@@ -99,46 +99,82 @@ var (
 func (p *SuperProxy) writeProxyReq(c net.Conn, targetHostWithPort []byte) error {
 	buf := bytebufferpool.Get()
 	defer bytebufferpool.Put(buf)
-	buf.B = make([]byte, len(superProxyReqMethod)+lensuperProxyReqSP+
-		len(targetHostWithPort)+lensuperProxyReqSP+
-		len(superProxyReqProtocol)+lensuperProxyReqCRLF+
-		len(superProxyReqHostHeader)+lensuperProxyReqSP+
-		len(targetHostWithPort)+lensuperProxyReqCRLF+
-		len(p.authHeaderWithCRLF)+lensuperProxyReqCRLF)
-
+	buf.B = make([]byte, len(superProxyReqMethod)+len(superProxyReqSP)+
+		len(targetHostWithPort)+len(superProxyReqSP)+
+		len(superProxyReqProtocol)+len(superProxyReqCRLF)+
+		len(superProxyReqHostHeader)+len(superProxyReqSP)+
+		len(targetHostWithPort)+len(superProxyReqCRLF)+
+		len(p.authHeaderWithCRLF)+len(superProxyReqCRLF))
 	copyIndex := 0
 	copyBytes := func(b []byte) {
 		copy(buf.B[copyIndex:], b)
 		copyIndex += len(b)
 	}
-	copySuperProxyReqSP := func() {
-		copy(buf.B[copyIndex:], superProxyReqSP)
-		copyIndex += lensuperProxyReqSP
-	}
-	copySuperProxyReqCRLF := func() {
-		copy(buf.B[copyIndex:], superProxyReqCRLF)
-		copyIndex += lensuperProxyReqCRLF
-	}
 	copyBytes(superProxyReqMethod)
-	copySuperProxyReqSP()
+	copyBytes(superProxyReqSP)
 	copyBytes(targetHostWithPort)
-	copySuperProxyReqSP()
+	copyBytes(superProxyReqSP)
 	copyBytes(superProxyReqProtocol)
-	copySuperProxyReqCRLF()
+	copyBytes(superProxyReqCRLF)
 	copyBytes(superProxyReqHostHeader)
-	copySuperProxyReqSP()
+	copyBytes(superProxyReqSP)
 	copyBytes(targetHostWithPort)
-	copySuperProxyReqCRLF()
+	copyBytes(superProxyReqCRLF)
 	copyBytes(p.authHeaderWithCRLF)
-	copySuperProxyReqCRLF()
+	copyBytes(superProxyReqCRLF)
 	return util.WriteWithValidation(c, buf.B)
 }
 
 //readProxyReq reads proxy connection request result (i.e. response)
 //only 200 OK is accepted.
-func (p *SuperProxy) readProxyReq(c net.Conn, pool *bufiopool.Pool) error {
+func (p *SuperProxy) readProxyResp(c net.Conn, pool *bufiopool.Pool) error {
 	r := pool.AcquireReader(c)
 	defer pool.ReleaseReader(r)
-
-	return nil
+	n := 1
+	isStartLine := true
+	headerParsed := false
+	for {
+		if b, err := r.Peek(n); err != nil {
+			return err
+		} else if len(b) == 0 {
+			return io.EOF
+		}
+		//must read buffed bytes
+		b, err := r.Peek(r.Buffered())
+		if len(b) == 0 || err != nil {
+			return fmt.Errorf("bufio.Reader.Peek() returned unexpected data (%q, %v)", b, err)
+		}
+		//read and discard every header line
+		m := 0
+		for !headerParsed {
+			b := b[m:]
+			lineLen := bytes.IndexByte(b, '\n')
+			if lineLen < 0 {
+				//need more
+				break
+			}
+			lineLen++
+			m += lineLen
+			if isStartLine {
+				isStartLine = false
+				if !bytes.Contains(b[:lineLen], []byte(" 200 ")) {
+					return fmt.Errorf("connected to proxy failed with startline %s", b[:lineLen])
+				}
+			} else {
+				if (lineLen == 2 && b[0] == '\r') || lineLen == 1 {
+					//single \n or \r\n means end of the header
+					headerParsed = true
+				}
+			}
+			if _, err := r.Discard(lineLen); err != nil {
+				return fmt.Errorf("bufio.Reader.Discard(%d) failed: %s", lineLen, err)
+			}
+		}
+		if headerParsed {
+			//TODO: discard http body also? Does the proxy connect response contains body?
+			return nil
+		}
+		//require one more byte
+		n = r.Buffered() + 1
+	}
 }
