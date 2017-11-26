8ca25890a9d5a4b5482370a9f3743598537df2b8
Zichao LI <zichao@haxii.com>
2017-11-26 10:25:18 +0800

    hijacked client added

--- hijack/hijacker.go
+++ hijack/hijacker.go
@@ -1,36 +1,35 @@
 package hijack
 
 import (
+	"bufio"
 	"io"
 	"net"
 
 	"github.com/haxii/fastproxy/http"
 )
 
-//Hijacker http connection hijacker
+//Hijacker hijacker of each http connection
 // Sniffer: `OnRequest` & `OnResponse`
-// Modifer:
+// Modifer: `HijackResponse`
 type Hijacker interface {
 	// OnRequest give the request header in parameters then
 	// write request body in the writer returned
-	OnRequest(host string, method, path []byte,
-		header http.Header, rawHeader []byte) io.Writer
+	OnRequest(header http.Header, rawHeader []byte) io.Writer
 
 	// OnResponse give the response header in parameters then
 	// write response body in the writer returned
-	OnResponse(statusCode int,
-		header http.Header, rawHeader []byte) io.Writer
+	OnResponse(statusCode int, header http.Header, rawHeader []byte) io.Writer
 
 	// HijackResponse return a response hijacking reader
 	//  a non-nil reader means proxy would stop the request to target
 	//  server then return the reader's response
-	HijackResponse() io.Reader
+	HijackResponse() *bufio.Reader
 }
 
 //HijackerPool pooling hijacker instances
 type HijackerPool interface {
 	// Get get a hijacker with client address
-	Get(net.Addr) Hijacker
+	Get(clientAddr net.Addr, host string, method, path []byte) Hijacker
 	// Put put a hijacker back to pool
 	Put(Hijacker)
 }
