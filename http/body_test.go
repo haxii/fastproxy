cb70ccc41141193823ece7c7588c40d635501f37
xiangyu <xiangyu@haxii.com>
2018-03-08 12:20:10 +0800

    update

--- http/body_test.go
+++ http/body_test.go
@@ -1,15 +1,24 @@
 package http
 
 import (
+	"bufio"
+	"strings"
 	"testing"
 )
 
 func TestParseBodyField(t *testing.T) {
-	testParseBodyFieldByBodyType(t, BodyTypeChunked)
-	testParseBodyFieldByBodyType(t, BodyTypeFixedSize)
-	testParseBodyFieldByBodyType(t, BodyTypeIdentity)
+	testParseBodyFieldByBodyType(t, BodyTypeChunked, "5\r\nasdfg\r\n0\r\n\r\n")
+	testParseBodyFieldByBodyType(t, BodyTypeFixedSize, "sdfzdfsdfewrfsdf\r\n\r\n")
+	testParseBodyFieldByBodyType(t, BodyTypeIdentity, "sdfzdfsdfewrfsdf\r\n\r\n")
 }
 
-func testParseBodyFieldByBodyType(t *testing.T, btc BodyType) {
-
+func testParseBodyFieldByBodyType(t *testing.T, bt BodyType, s string) {
+	body := &Body{}
+	br := bufio.NewReader(strings.NewReader(s))
+	w := func(isChunkHeader bool, data []byte) error {
+		return nil
+	}
+	if err := body.Parse(br, bt, int64(len(s)), w); err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
 }
---
+++ proxy/proxy/proxy_test.go
@@ -0,0 +1,35 @@
+package proxy
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"testing"
+)
+
+func TestProxyServe(t *testing.T) {
+	proxy := &Proxy{}
+	ln, err := net.Listen("tcp", "0.0.0.0:5050")
+	if err != nil {
+		t.Fatalf("unexpect error: %s", err)
+	}
+	go func() {
+		if err = proxy.Serve(ln); err != nil {
+			t.Fatalf("unexpect error: %s", err)
+		}
+	}()
+
+	conn, err := net.Dial("tcp", "0.0.0.0:5050")
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	fmt.Fprintf(conn, "GET / HTTP/1.1\r\n\r\n")
+	status, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+	if status != "400" {
+		t.Fatalf("an error occurred when send get request")
+	}
+}
