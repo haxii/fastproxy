529af233afe057a775ec941262851698392187b0
xiangyu <xiangyu@haxii.com>
2018-03-12 17:52:39 +0800

    rewrite pool_test for bytebufferpool and fixed some error for bufiopool

--- bufiopool/pool_test.go
+++ bufiopool/pool_test.go
@@ -1,6 +1,7 @@
 package bufiopool
 
 import (
+	"io"
 	"strings"
 	"testing"
 
@@ -13,9 +14,24 @@ func TestBufioPool(t *testing.T) {
 	for i := 0; i < 5; i++ {
 		nr := newPool.AcquireReader(nReader)
 		if nr.Buffered() != 0 {
-			t.Fatal("Bufiopool can't acquire a empty reader")
+			t.Fatal("Bufiopool can't acquire an empty reader")
+		}
+		newByte := make([]byte, 10)
+		result, err := nr.Read(newByte)
+		if err != nil && err != io.EOF {
+			t.Fatalf("unexpected error: %s", err.Error())
+		}
+		if i > 0 && result != 0 {
+			t.Fatal("Bufiopool can't acquire an empty reader")
 		}
 		newPool.ReleaseReader(nr)
+		result, err = nr.Read(newByte)
+		if err != nil && err != io.EOF {
+			t.Fatalf("unexpected error: %s", err.Error())
+		}
+		if result != 0 {
+			t.Fatal("Bufiopool can't release a reader")
+		}
 	}
 
 	newWriter := bytebufferpool.Get()
@@ -23,7 +39,7 @@ func TestBufioPool(t *testing.T) {
 	for i := 0; i < 5; i++ {
 		nw := newPool.AcquireWriter(newWriter)
 		if nw.Buffered() != 0 {
-			t.Fatal("Bufiopool can't acquire a empty writer")
+			t.Fatal("Bufiopool can't acquire an empty writer")
 		}
 		_, err := nw.WriteString("123")
 		if err != nil {
