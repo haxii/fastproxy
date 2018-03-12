8c9a3aa834d9770aad26c4b90f71b2399304b16e
xiangyu <xiangyu@haxii.com>
2018-03-12 12:06:20 +0800

    update

--- proxy/http/http_test.go
+++ proxy/http/http_test.go
@@ -3,8 +3,14 @@ package http
 import (
 	"bufio"
 	"bytes"
+	"io"
+	"net"
 	"strings"
 	"testing"
+
+	"github.com/haxii/fastproxy/bufiopool"
+	"github.com/haxii/fastproxy/bytebufferpool"
+	"github.com/haxii/fastproxy/http"
 )
 
 func TestHttpRequest(t *testing.T) {
@@ -32,8 +38,6 @@ func TestHttpRequest(t *testing.T) {
 	}
 }
 
-// TODO: run test pass
-/*
 func TestHttpResponse(t *testing.T) {
 	s := "HTTP/1.1 200 ok\r\n" +
 		"Cache-Control:no-cache\r\n" +
@@ -42,14 +46,63 @@ func TestHttpResponse(t *testing.T) {
 	bPool := bufiopool.New(1, 1)
 	br := bPool.AcquireReader(strings.NewReader(s))
 	byteBuffer := bytebufferpool.MakeFixedSizeByteBuffer(100)
-	err := resp.ReadFrom(true, br)
+	bw := bufio.NewWriter(byteBuffer)
+	err := resp.WriteTo(bw)
 	if err != nil {
 		t.Fatalf("unexpected error: %s", err)
 	}
-	bw := bufio.NewWriter(byteBuffer)
-	err = resp.WriteTo(bw)
-	if err != nil {
-		t.Fatalf("unexpected error: %s", err)
+
+	sHijacker := &hijacker{}
+	addr := testAddr{netWork: "tcp", clientAddr: "127.0.0.1:10000"}
+	var clientAddr net.Addr = &addr
+	sHijacker.Set(clientAddr, "localhost", []byte("GET"), []byte("/"))
+
+	resp.SetHijacker(sHijacker)
+
+	err = resp.ReadFrom(false, br)
+	if resp.GetSize() != 43 {
+		t.Fatal("Response read from reader failed")
+	}
+	if resp.ConnectionClose() {
+		t.Fatal("Response connection close is wrong")
 	}
 }
-*/
+
+type testAddr struct {
+	clientAddr string
+	netWork    string
+}
+
+func (a *testAddr) String() string {
+	return a.clientAddr
+}
+
+func (a *testAddr) Network() string {
+	return a.netWork
+}
+
+type hijacker struct {
+	clientAddr, targetHost string
+	method, path           []byte
+}
+
+func (s *hijacker) Set(clientAddr net.Addr,
+	host string, method, path []byte) {
+	s.clientAddr = clientAddr.String()
+	s.targetHost = host
+	s.method = method
+	s.path = path
+}
+
+func (s *hijacker) OnRequest(header http.Header, rawHeader []byte) io.Writer {
+	return nil
+}
+
+func (s *hijacker) HijackResponse() io.Reader {
+	return nil
+}
+
+func (s *hijacker) OnResponse(respLine http.ResponseLine,
+	header http.Header, rawHeader []byte) io.Writer {
+	return nil
+}
