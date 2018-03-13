c771ab88694a2ebb5c5022e99a29dbac42122df5
xiangyu <xiangyu@haxii.com>
2018-03-13 10:45:47 +0800

    change cert.go

--- cert/cert.go
+++ cert/cert.go
@@ -138,21 +138,43 @@ func MakeClientTLSConfig(host, serverName string) *tls.Config {
 }
 
 // MakeClientTLSConfigByCA make a client TLS config based on self-signed ca certificate
-func MakeClientTLSConfigByCA(filePath string) *tls.Config {
+func MakeClientTLSConfigByCA(host, serverName, filePath string) *tls.Config {
+	tlsServerName := func(addr string) string {
+		if !strings.Contains(addr, ":") {
+			return addr
+		}
+		host, _, err := net.SplitHostPort(addr)
+		if err != nil {
+			return "*"
+		}
+		return host
+	}
+	tlsConfig := &tls.Config{}
+	tlsConfig.ClientSessionCache = tls.NewLRUClientSessionCache(0)
+
+	if len(serverName) == 0 {
+		hostName := tlsServerName(host)
+		if hostName == "*" {
+			tlsConfig.InsecureSkipVerify = true
+		} else {
+			tlsConfig.ServerName = hostName
+		}
+	} else {
+		tlsConfig.ServerName = serverName
+	}
 	selfCertificate, err := ioutil.ReadFile(filePath)
 	if err != nil {
-		return nil
+		return tlsConfig
 	}
 	newCert := &tls.Certificate{}
 	key, err := genKeyPair()
 	if err != nil {
-		return nil
+		return tlsConfig
 	}
 	newCert.Certificate = append(newCert.Certificate, selfCertificate)
 	newCert.PrivateKey = key
 	newCert.Leaf, _ = x509.ParseCertificate(selfCertificate)
 
-	tlsConfig := &tls.Config{}
 	tlsConfig.Certificates = append(tlsConfig.Certificates, *newCert)
 	return tlsConfig
 }
