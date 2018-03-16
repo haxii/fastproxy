ecd6f3d680fc4632d22ec3e0f0df5653129cc6f3
xiangyu <xiangyu@haxii.com>
2018-03-16 15:04:50 +0800

    update

--- proxy/proxy/servershutdown_test.go
+++ proxy/proxy/servershutdown_test.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-func TestServerShutDownByRightParamters(t *testing.T) {
+func TestServerShutDownByRightParameters(t *testing.T) {
 	ln, err := net.Listen("tcp", "0.0.0.0:7777")
 	if err != nil {
 		t.Fatalf("unexpected error: %s", err)
@@ -47,26 +47,43 @@ func TestServerShutDownByRightParamters(t *testing.T) {
 	if err = gln.Close(); err != nil {
 		t.Fatalf("unexpected error: %s", err)
 	}
+	if conn, err = gln.Accept(); err == nil {
+		t.Fatalf("expected error: use of closed network connection, gln do not close")
+	}
 }
 
-func TestServerShutDownByWrongParamters(t *testing.T) {
+func TestServerShutDownByWrongParameters(t *testing.T) {
+	ln, err := net.Listen("tcp", "0.0.0.0:7777")
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
 	gln := NewGracefulListener(nil, 3*time.Second)
+	if gln != nil {
+		t.Fatalf("gln should be nil")
+	}
+
+	gln = NewGracefulListener(ln, 3*time.Second)
+	_, err = net.Dial("tcp4", "0.0.0.0:7777")
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
 	gConn, err := gln.Accept()
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+	err = gln.Close()
 	if err == nil {
-		t.Fatalf("expected error: graceful listener is nil")
+		t.Fatalf("expected error: cannot complete graceful shutdown")
 	}
-	if !strings.Contains(err.Error(), "graceful listener is nil") {
+	if strings.Contains(err.Error(), "expected error: cannot complete graceful shutdown") {
+		t.Fatalf("unexpected error: %s", err.Error())
+	}
+	err = gConn.Close()
+	if err != nil {
 		t.Fatalf("unexpected error: %s", err)
 	}
-	b := make([]byte, 20)
-	_, err = gConn.Read(b)
-	if !bytes.Contains(b, []byte("Hello, world!")) {
-		t.Fatalf("unexpected data read: %s, expected data is 'Hello, world!' ", string(b))
-	}
-	if err := gConn.Close(); err != nil {
-		t.Fatalf("unexpected error: %s", err)
-	}
-	if err = gln.Close(); err != nil {
+	err = gln.Close()
+	if err != nil {
 		t.Fatalf("unexpected error: %s", err)
 	}
 }
